82d435727a1f49327bc7ef8cde31008afc8f8ee7
ichang0301 <oeh900301@gmail.com>
2022-02-22 11:06:23 +0900

    refactor 'dependancy-injection'

--- 8_dependancy-injection/dependancy_injection.go
+++ 8_dependancy-injection/dependancy_injection.go
@@ -6,5 +6,5 @@ import (
 )
 
 func Greet(writer io.Writer, name string) { // fmt.Fprintf allows you to pass in an io.Writer which we know both os.Stdout and bytes.Buffer implement.
-	fmt.Fprintf(writer, "Hello, %s", name)
+	fmt.Fprintf(writer, "Hello, %s\n", name)
 }
