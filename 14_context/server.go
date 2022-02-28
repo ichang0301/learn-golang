fd451571e850f3471662ee239092254b4782bb21
ichang0301 <oeh900301@gmail.com>
2022-02-28 14:49:53 +0900

    refactor Server() that it sends a context through to the downstream Store and that it handles the error that will come from the Store when it is canceled.

--- 14_context/server.go
+++ 14_context/server.go
@@ -3,33 +3,27 @@
 // go doc: https://pkg.go.dev/context
 // go blog: https://go.dev/blog/context
 
+// 'Context.Value' should inform, not control. If a function needs some values, put them as typed parameters rather than trying to fetch them from context.Value.
+
 package contexts
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 )
 
 type Store interface {
-	Fetch() string
-	Cancel()
+	Fetch(ctx context.Context) (string, error)
 }
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-
-		data := make(chan string, 1)
-
-		go func() {
-			data <- store.Fetch()
-		}()
-
-		select {
-		case d := <-data:
-			fmt.Fprint(w, d)
-		case <-ctx.Done():
-			store.Cancel()
+		data, err := store.Fetch(r.Context()) // The server code has become simplified as it's no longer explicitly responsible for cancellation, it simply passes through context and relies on the downstream functions to respect any cancellations that may occur.
+		if err != nil {
+			return // todo: log error however you like
 		}
+
+		fmt.Fprint(w, data)
 	}
 }
