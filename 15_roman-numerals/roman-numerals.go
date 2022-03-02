40cbe9a377fb3ee04d94bc180e95fcbf58505a0a
ichang0301 <oeh900301@gmail.com>
2022-03-02 14:10:04 +0900

    [WIP] add converter from Roman to Arabic but only fits up to 4 and it's horrible

--- 15_roman-numerals/roman-numerals.go
+++ 15_roman-numerals/roman-numerals.go
@@ -9,7 +9,19 @@ type RomanNumeral struct {
 	Symbol string
 }
 
-var allRomanNumerals = []RomanNumeral{
+type RomanNumerals []RomanNumeral
+
+func (r RomanNumerals) ValueOf(symbol string) int {
+	for _, s := range r {
+		if s.Symbol == symbol {
+			return s.Value
+		}
+	}
+
+	return 0
+}
+
+var allRomanNumerals = RomanNumerals{
 	{Value: 1000, Symbol: "M"},
 	{Value: 900, Symbol: "CM"},
 	{Value: 500, Symbol: "D"},
@@ -26,7 +38,6 @@ var allRomanNumerals = []RomanNumeral{
 }
 
 func ConvertToRoman(arabic int) string {
-
 	var result strings.Builder
 
 	for _, numeral := range allRomanNumerals {
@@ -37,4 +48,36 @@ func ConvertToRoman(arabic int) string {
 	}
 
 	return result.String()
+}
+
+func ConvertToArabic(roman string) int {
+	total := 0
+	for i := 0; i < len(roman); i++ {
+		symbol := roman[i]
+
+		// look ahead to next symbol if we can and the current symbol is base 10 (only valid subtractors)
+		if couldBeSubtractive(i, symbol, roman) {
+			nextSymbol := roman[i+1]
+
+			// build the two character string
+			potentialNumber := string([]byte{symbol, nextSymbol})
+
+			// get the value of the two character string
+			value := allRomanNumerals.ValueOf(potentialNumber)
+
+			if value != 0 {
+				total += value
+				i++ // move past this character too for the next loop
+			} else {
+				total++
+			}
+		} else {
+			total++
+		}
+	}
+	return total
+}
+
+func couldBeSubtractive(index int, currentSymbol uint8, roman string) bool {
+	return index+1 < len(roman) && currentSymbol == 'I'
 }
