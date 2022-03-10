a7b5d9eba0bb0e9b2d24572b80f646ed04166b48
ichang0301 <oeh900301@gmail.com>
2022-03-10 12:07:18 +0900

    [WIP] handle read dir failing

--- 17_reading_files/blogposts.go
+++ 17_reading_files/blogposts.go
@@ -4,8 +4,11 @@ import (
 	"io/fs"
 )
 
-func PostsFromFS(fileSystem fs.FS) []Post {
-	dir, _ := fs.ReadDir(fileSystem, ".")
+func PostsFromFS(fileSystem fs.FS) ([]Post, error) {
+	dir, err := fs.ReadDir(fileSystem, ".")
+	if err != nil {
+		return nil, err
+	}
 
 	var posts []Post
 	for _, f := range dir {
@@ -13,7 +16,7 @@ func PostsFromFS(fileSystem fs.FS) []Post {
 		posts = append(posts, post)
 	}
 
-	return posts
+	return posts, nil
 }
 
 func makePostFromFile(fileSystem fs.FS, fileName string) Post {
--- 17_reading_files/blogposts_test.go
+++ 17_reading_files/blogposts_test.go
@@ -1,6 +1,8 @@
 package blogposts_test
 
 import (
+	"errors"
+	"io/fs"
 	"reflect"
 	"testing"
 	"testing/fstest"
@@ -9,22 +11,42 @@ import (
 )
 
 func TestBlogPosts(t *testing.T) {
-	// Given
-	fs := fstest.MapFS{
-		"hello-world.md": {Data: []byte("Title: Hello, TDD world!")},
-		// "hello-go.md":    {Data: []byte("Title: Hello, go!")},
-	}
+	t.Run("happy path", func(t *testing.T) {
+		// Given
+		fs := fstest.MapFS{
+			"hello-world.md": {Data: []byte("Title: Hello, TDD world!")},
+			// "hello-go.md":    {Data: []byte("Title: Hello, go!")},
+		}
 
-	// When
-	posts := blogposts.PostsFromFS(fs)
+		// When
+		posts, err := blogposts.PostsFromFS(fs)
 
-	// Then
-	if len(posts) != len(fs) {
-		t.Errorf("expected %d posts, got %d posts", len(fs), len(posts))
-	}
+		// Then
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	expectedFirstPost := blogposts.Post{Title: "Hello, TDD world!"}
-	if !reflect.DeepEqual(posts[0], expectedFirstPost) {
-		t.Errorf("got %#v, want %#v", posts[0], expectedFirstPost)
-	}
+		if len(posts) != len(fs) {
+			t.Errorf("expected %d posts, got %d posts", len(fs), len(posts))
+		}
+
+		expectedFirstPost := blogposts.Post{Title: "Hello, TDD world!"}
+		if !reflect.DeepEqual(posts[0], expectedFirstPost) {
+			t.Errorf("got %#v, want %#v", posts[0], expectedFirstPost)
+		}
+	})
+
+	t.Run("failing fs", func(t *testing.T) {
+		_, err := blogposts.PostsFromFS(FailingFS{})
+		if err == nil {
+			t.Error("expected an error, didn't get one")
+		}
+	})
+}
+
+type FailingFS struct {
+}
+
+func (f FailingFS) Open(name string) (fs.File, error) {
+	return nil, errors.New("this function always fail")
 }
