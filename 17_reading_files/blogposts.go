9859e6c00533ccef44b9ac3e9e6f5fd7051613d0
ichang0301 <oeh900301@gmail.com>
2022-03-10 10:48:42 +0900

    [WIP] refactoring

--- 17_reading_files/blogposts.go
+++ 17_reading_files/blogposts.go
@@ -1,33 +1,22 @@
 package blogposts
 
 import (
-	"io"
 	"io/fs"
-	"strings"
 )
-
-type Post struct {
-	Title string
-}
 
 func PostsFromFS(fileSystem fs.FS) []Post {
 	dir, _ := fs.ReadDir(fileSystem, ".")
 
 	var posts []Post
 	for _, f := range dir {
-		post := makePostFromFile(fileSystem, f)
+		post := makePostFromFile(fileSystem, f.Name())
 		posts = append(posts, post)
 	}
 
 	return posts
 }
 
-func makePostFromFile(fileSystem fs.FS, f fs.DirEntry) Post {
-	blogFile, _ := fileSystem.Open(f.Name())
-	fileContents, _ := io.ReadAll(blogFile)
-	title := strings.TrimPrefix(string(fileContents), "Title: ")
-
-	return Post{
-		Title: title,
-	}
+func makePostFromFile(fileSystem fs.FS, fileName string) Post {
+	blogFile, _ := fileSystem.Open(fileName)
+	return newPost(blogFile)
 }
