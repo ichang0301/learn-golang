5c6f112a44976a8345152ef2075065df6fb24956
ichang0301 <oeh900301@gmail.com>
2022-03-18 19:56:52 +0900

    [WIP] refactor to move template into a file for index page and add approval test

--- 18_templating/renderer.go
+++ 18_templating/renderer.go
@@ -25,6 +25,10 @@ type Post struct {
 	Tags                     []string
 }
 
+func (p Post) SanitisedTitle() string {
+	return strings.ToLower(strings.Replace(p.Title, " ", "-", -1))
+}
+
 type PostRenderer struct {
 	templ *template.Template
 }
@@ -39,29 +43,9 @@ func NewPostRenderer() (*PostRenderer, error) {
 }
 
 func (r *PostRenderer) Render(w io.Writer, p Post) error {
-
-	if err := r.templ.Execute(w, p); err != nil {
-		return err
-	}
-
-	return nil
+	return r.templ.ExecuteTemplate(w, "blog.gohtml", p) // By parsing more than one template into templ we now have to call ExecuteTemplate and specify which template we wish to render as appropriate.
 }
 
 func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
-	indexTemplate := `<ol>{{range .}}<li><a href="/post/{{sanitiseTitle .Title}}">{{.Title}}</a></li>{{end}}</ol>`
-
-	templ, err := template.New("index").Funcs(template.FuncMap{
-		"sanitiseTitle": func(title string) string {
-			return strings.ToLower(strings.Replace(title, " ", "-", -1))
-		},
-	}).Parse(indexTemplate)
-	if err != nil {
-		return err
-	}
-
-	if err := templ.Execute(w, posts); err != nil {
-		return err
-	}
-
-	return nil
+	return r.templ.ExecuteTemplate(w, "index.gohtml", posts)
 }
