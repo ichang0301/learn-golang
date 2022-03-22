34613dffe166d800b9685b010b57ba8e37962a08
ichang0301 <oeh900301@gmail.com>
2022-03-22 13:20:53 +0900

    [WIP] handle the scenario where the player doesn't exist

--- 19_http_server/server.go
+++ 19_http_server/server.go
@@ -18,7 +18,12 @@ type PlayerServer struct {
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) { // The Handler interface is what we need to implement in order to make a server, and the HandlerFunc type is an adapter to allow the use of ordinary functions as HTTP handlers. HandlerFunc documentation: https://pkg.go.dev/net/http#HandlerFunc and Handler documentation: https://pkg.go.dev/net/http#Handler
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
 
-	fmt.Fprint(w, p.Store.GetPlayerScore(player)) // ResponseWriter also implements io Writer so we can use fmt.Fprint to send strings as HTTP responses.
+	score := p.Store.GetPlayerScore(player)
+	if score == 0 {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	fmt.Fprint(w, score) // ResponseWriter also implements io Writer so we can use fmt.Fprint to send strings as HTTP responses.
 }
 
 func GetPlayerScore(name string) int {
