b90d01554fc2031edf8365538d65cf71c80b41a3
ichang0301 <oeh900301@gmail.com>
2022-03-29 09:08:03 +0900

    [WIP] feature to check integrates with the PlayerStore how we'd like.

---
+++ 22_command_line/CLI.go
@@ -0,0 +1,9 @@
+package command_line
+
+type CLI struct {
+	store PlayerStore
+}
+
+func (c *CLI) PlayPoker() {
+	c.store.RecordWin("name")
+}
---
+++ 22_command_line/CLI_test.go
@@ -0,0 +1,13 @@
+package command_line
+
+import "testing"
+
+func TestCLI(t *testing.T) {
+	playerStore := &StubPlayerStore{}
+	cli := &CLI{playerStore}
+	cli.PlayPoker()
+
+	if len(playerStore.winCalls) != 1 {
+		t.Fatal("expected a win call but didn't get any")
+	}
+}
