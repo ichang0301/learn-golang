7a0c82c294071e9cf6a37e16b541a3b9a54a4971
ichang0301 <oeh900301@gmail.com>
2022-03-29 09:01:16 +0900

    [WIP] init commit before starting 'Command line & package structure' chapter

---
+++ 22_command_line/tape.go
@@ -0,0 +1,16 @@
+package command_line
+
+import (
+	"os"
+)
+
+type tape struct {
+	// file io.ReadWriteSeeker // io.ReadWriteSeeker is embedding io.Reader, io.Writer and io.Seeker interface
+	file *os.File
+}
+
+func (t *tape) Write(p []byte) (n int, err error) {
+	t.file.Truncate(0) // os.File has a truncate function that will let us effectively empty the file.
+	t.file.Seek(0, 0)
+	return t.file.Write(p)
+}
